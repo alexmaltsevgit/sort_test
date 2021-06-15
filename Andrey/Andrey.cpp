dfd62b04e7303cff601deb77bf949018c72ba68c
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-06-15 16:30:17 +0300

    fix

--- Andrey/Andrey.cpp
+++ Andrey/Andrey.cpp
@@ -1,19 +1,32 @@
 ﻿#include "TestSort.h"
 #include "QuickSort.h"
+#include "MergeSort.h"
+#include "NaturalMergeSort.h"
+#include "RadixSort.h"
 #include <filesystem>
 
-#define N1 10000
-#define N2 60000
-#define N3 110000
-#define N4 160000
+enum class Modes : int {
+	N1 = 10000,
+	N2 = 60000,
+	N3 = 110000,
+	N4 = 160000
+};
 
 int main() {
+	// change this to test for another array size
+	constexpr int mode = static_cast<int>(Modes::N1);
+
+	// change this to choose another path
 	std::filesystem::path path = "../";
+
 	std::string absolute_path = std::filesystem::absolute(path).string();
 	TestSort test{
-		new QuickSort()
+		new QuickSort(),
+		new MergeSort(),
+		new NaturalMergeSort(),
+		new RadixSort()
 	};
 
-	test.generate_all_files(N1, absolute_path);
-	test.start(N1);
+	test.generate_all_files(mode, absolute_path);
+	test.start(mode);
 }
